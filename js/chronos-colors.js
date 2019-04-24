d65a4d0c0a46829d86b04688e4639063451feff7
mierenga <mike.swierenga@gmail.com>
2019-04-23 22:46:12 -0400

    Added two color palletes for future reference from the Crayola catalog

--- js/chronos-colors.js
+++ js/chronos-colors.js
@@ -1,3 +1,4 @@
+// cool it
 colors = [
 '#A3CB38',
 '#0C7D00',
@@ -10,6 +11,44 @@ colors = [
 'blueviolet',
 '#6F1E51',
 '#5758BB',
+];
+
+// star brite (crayola)
+colors=[
+  '#F898C8',
+  '#E91E63',
+  '#D62518',
+  '#AD0000',
+  '#FA7A00',
+  '#CDDC39',
+  '#00D8A0',
+  '#1BA77B',
+  '#004C71',
+  '#1AADE0',
+  '#0069BD',
+  '#333399',
+  '#56418C',
+  '#212321',
+  '#E63300',
+  '#DE6900',
+];
+
+// construction paper (crayola)
+xcolors=[
+  '#FFA3B1',
+  '#F3715A',
+  '#F37B70',
+  '#FFAD59',
+  '#FFE599',
+  '#F8FC98',
+  '#B4FFB4',
+  '#00CC99',
+  '#00BCD4',
+  '#03A9F4',
+  '#4848FF',
+  '#6A35CE',
+  '#AA55AA',
+  '#7F7FBF',
 ];
 
 document.chronos_colors = colors;
