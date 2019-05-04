9a5f4b9e7413124ebe1a66f854781d4d0e7b598e
Mike Swierenga <mike.swierenga@gmail.com>
2019-05-04 15:48:14 -0400

    Added the start for a menu in the popup

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+window.onload = function() {
+  addGUI();
+}
+
 simplify();
+// Automatically apply the simplify feature when the extension is clicked
+
+
+console.log('did we get \'em?');
+console.log(controls-gui);
+console.log(State);
 
 function simplify() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -13,3 +23,33 @@ function simplify() {
         {file: '/js/simplify.js'});
   });
 }
+
+function addGUI() {
+  let controls = new BasicControls();
+  let gui = new dat.GUI();
+  gui.add(controls, 'use_colors');
+  gui.add(controls, 'color_schemes');
+  gui.add(controls, 'text_size', 4, 64).step(1);
+}
+
+let BasicControls = function() {
+  this.use_colors = true;
+  this.text_size = 24;
+  this.color_schemes = ['chill_out', 'star_brite', 'construction_paper'];
+}
+
+function wrapGUI (state, opts) {
+  const root = document.createElement('div');
+  const gui = GUI(state, Object.assign({
+      root: root,
+      containerCSS: "max-width:350px;padding:30px 0;",
+      theme: Object.assign({}, (opts || {}).theme, {
+        fontFamily: "'Helvetica', sans-serif",
+        fontSize: '13px',
+      }),
+    }, opts || {}))
+    .$onChanges(e => root.dispatchEvent(new CustomEvent("input")));
+  root.value = state;
+  return root;
+}
+
